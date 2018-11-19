eb0e4471b19482c87eb46b3c00d9963fa41087da
yuxi <358819135@qq.com>
2018-11-19 16:59:28 +0800

    add data.js

---
+++ data.js
@@ -0,0 +1,134 @@
+let allData = {
+  mainData: [
+    {
+      title: '今日待还款总金额(不含逾期)',
+      num: 10000.00
+    },
+    {
+      title: '全部待还款总金额(不含逾期)',
+      num: 2100.00
+    },
+    {
+      title: '大盘总待还金额(包括逾期)',
+      num: 12000.00
+    }
+  ],
+  register: {
+    date: {
+      name: '日期',
+      data: ['12/11', '13/11', '14/11', '15/11', '16/11', '17/11', '18/11']
+    },
+    num: {
+      name: '注册量',
+      data: [0, 10, 32, 46, 32, 45, 98]
+    },
+    entry: {
+      name: '进件量',
+      data: [0, 0, 14, 25, 26, 31, 55]
+    },
+    pass: {
+      name: '审核通过量',
+      data: [0, 0, 0, 6, 7, 20, 56]
+    }
+  },
+  titleData: [
+    {
+      title: '放款人数',
+      today: 120,
+      yesterday: 290
+    },
+    {
+      title: '放款金额',
+      today: 12000,
+      yesterday: 139988
+    },
+    {
+      title: '逾期人数',
+      today: 20,
+      yesterday: 12
+    },
+    {
+      title: '逾期金额',
+      today: 8000,
+      yesterday: 12000
+    },
+    {
+      title: '盈利金额',
+      today: 9998,
+      yesterday: 12000
+    }
+  ],
+  overdueRate: {
+    ageRatio: [
+      {
+        title: '19岁以下',
+        num: 120
+      },
+      {
+        title: '20-30岁',
+        num: 500
+      },
+      {
+        title: '31-40岁',
+        num: 320
+      },
+      {
+        title: '41岁以上',
+        num: 30
+      }
+    ],
+    date: ['12/11', '13/11', '14/11', '15/11', '16/11', '17/11', '18/11'],
+    rate: [10.12, 8.09, 5.21, 1.12, 2.09, 13.21, 6.32]
+  },
+  dataCount: [
+    {
+      title: '注册用户',
+      history: 123333,
+      year: 82333,
+      month: 1200,
+      week: 123
+    },
+    {
+      title: '放款人数',
+      history: 123333,
+      year: 82333,
+      month: 1200,
+      week: 123
+    },
+    {
+      title: '放款金额',
+      history: 123333,
+      year: 82333,
+      month: 1200,
+      week: 123
+    },
+    {
+      title: '逾期人数',
+      history: 123333,
+      year: 82333,
+      month: 1200,
+      week: 123
+    },
+    {
+      title: '逾期金额',
+      history: 123333,
+      year: 82333,
+      month: 1200,
+      week: 123
+    },
+    {
+      title: '总逾期率',
+      history: 123333,
+      year: 82333,
+      month: 1200,
+      week: 123
+    },
+    {
+      title: '盈利金额',
+      history: 123333,
+      year: 82333,
+      month: 1200,
+      week: 123
+    }
+  ]
+}
